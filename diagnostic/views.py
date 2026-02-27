66f908a8f8ddb160989adbdf9903c7d4a64088b4
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-27 21:33:45 +0100

    Update views.py

--- diagnostic/views.py
+++ diagnostic/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
-from .models import Diagnostic
-from .serializers import DiagnosticSerializer
+from rest_framework import filters
 
 class DiagnosticViewSet(viewsets.ModelViewSet):
-    queryset = Diagnostic.objects.all().order_by('-created_at')
-    serializer_class = DiagnosticSerializer
+    queryset = DiagnosticEntry.objects.all().order_by('-created_at')
+    serializer_class = DiagnosticEntrySerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'symptoms', 'causes']
+    ordering_fields = ['created_at', 'name']
