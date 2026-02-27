d8200f93485a7bd58225f81efbe508e6dabd5e8d
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-27 21:39:49 +0100

    Update tests.py

--- diagnostic/tests.py
+++ diagnostic/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Category, DiagnosticEntry
 
-# Create your tests here.
+class DiagnosticEntryTestCase(TestCase):
+    def setUp(self):
+        self.cat = Category.objects.create(name='Crop')
+        self.entry = DiagnosticEntry.objects.create(
+            category=self.cat,
+            name='Tomato Blight',
+            symptoms='Leaves yellowing',
+            treatment='Fungicide application'
+        )
+
+    def test_str_method(self):
+        self.assertEqual(str(self.entry), 'Tomato Blight (Crop)')
