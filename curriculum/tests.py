0c22467fa12d12d0a5140fa28ed7abf309b3c15d
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-28 00:55:08 +0100

    Update tests.py

--- curriculum/tests.py
+++ curriculum/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Curriculum
 
-# Create your tests here.
+class CurriculumModelTest(TestCase):
+    def test_create_curriculum(self):
+        c = Curriculum.objects.create(
+            title='Tomato Farming',
+            description='Learn tomato cultivation',
+            duration='2 weeks',
+            level='Foundation'
+        )
+        self.assertEqual(str(c), 'Tomato Farming')
