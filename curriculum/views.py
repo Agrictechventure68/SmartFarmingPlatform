c549fbef28b9d8773eabe219d88ae6e6b6db02e0
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-28 00:51:34 +0100

    Update views.py

--- curriculum/views.py
+++ curriculum/views.py
@@ -2,6 +2,11 @@ from rest_framework import viewsets
 from .models import Curriculum
 from .serializers import CurriculumSerializer
 
+from rest_framework import filters
+
 class CurriculumViewSet(viewsets.ModelViewSet):
     queryset = Curriculum.objects.all()
     serializer_class = CurriculumSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'level']
+    ordering_fields = ['created_at', 'title']
