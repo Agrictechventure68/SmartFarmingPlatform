1af915eddf2917c74102524653f6e1330b93933c
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-27 21:38:38 +0100

    Update admin.py

--- diagnostic/admin.py
+++ diagnostic/admin.py
@@ -1,8 +1,18 @@
-from django.contrib import admin
-from .models import Diagnostic
+from .models import Category, DiagnosticEntry, DiagnosticHistory
 
-@admin.register(Diagnostic)
-class DiagnosticAdmin(admin.ModelAdmin):
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(DiagnosticEntry)
+class DiagnosticEntryAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'region', 'created_at')
     list_filter = ('category',)
     search_fields = ('name', 'symptoms', 'causes')
+
+@admin.register(DiagnosticHistory)
+class DiagnosticHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'diagnostic_entry', 'category', 'timestamp')
+    list_filter = ('category',)
+    search_fields = ('user__username',)
