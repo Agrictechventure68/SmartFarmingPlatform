e8d17b151c017e12c1363783f1b7951519bc1d9c
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-27 21:35:45 +0100

    Update urls.py

--- diagnostic/urls.py
+++ diagnostic/urls.py
@@ -1,7 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import DiagnosticViewSet
 
-router = DefaultRouter()
-router.register(r'', DiagnosticViewSet)
+router.register(r'', DiagnosticViewSet, basename='diagnostic-entry')
 
 urlpatterns = router.urls
