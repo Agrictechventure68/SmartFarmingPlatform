b319c4f339532860cce01e4ad6411c5dfb74e202
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-28 02:49:20 +0100

    Update serializers.py

--- diagnostic/serializers.py
+++ diagnostic/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Diagnostic
+from .models import DiagnosticEntry
+
 
 class DiagnosticEntrySerializer(serializers.ModelSerializer):
     class Meta:
