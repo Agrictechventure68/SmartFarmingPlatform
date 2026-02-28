7f9f92cc8bef51ab89362d334f36ea0cac606c95
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-28 02:05:47 +0100

    Update views.py

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,6 +2,11 @@ from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 
+from rest_framework import filters
+
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by('-added_on')
     serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'category']
+    ordering_fields = ['price', 'added_on']
