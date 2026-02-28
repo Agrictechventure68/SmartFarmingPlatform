73f435b020700e8d5951e2023a75c64c1f7f7e45
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-28 02:24:35 +0100

    Update views.py

--- farmers/views.py
+++ farmers/views.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets
 from .models import Farmer
 from .serializers import FarmerSerializer
 
+from rest_framework import filters
+
 class FarmerViewSet(viewsets.ModelViewSet):
-    queryset = Farmer.objects.all()
+    queryset = Farmer.objects.all().order_by('-registration_date')
     serializer_class = FarmerSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'location', 'farm_type']
