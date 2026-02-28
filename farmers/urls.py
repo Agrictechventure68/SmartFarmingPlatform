e5a42033e83e6d3a7f071addfc68e885b0457f51
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-28 02:26:22 +0100

    Update urls.py

--- farmers/urls.py
+++ farmers/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import FarmerViewSet
 
 router = DefaultRouter()
-router.register(r'', FarmerViewSet)
+router.register(r'', FarmerViewSet, basename='farmer')
 
 urlpatterns = router.urls
  
