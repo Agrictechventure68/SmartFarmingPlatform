4d0e1a836f31483067ce83d9ae93a7d9ebae6400
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-03-10 09:27:53 +0100

    Update admin.py

--- diagnostic/admin.py
+++ diagnostic/admin.py
@@ -1,18 +1,23 @@
-from .models import Category, DiagnosticEntry, DiagnosticHistory
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
-
-@admin.register(DiagnosticEntry)
-class DiagnosticEntryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'region', 'created_at')
-    list_filter = ('category',)
-    search_fields = ('name', 'symptoms', 'causes')
-
-@admin.register(DiagnosticHistory)
-class DiagnosticHistoryAdmin(admin.ModelAdmin):
-    list_display = ('user', 'diagnostic_entry', 'category', 'timestamp')
-    list_filter = ('category',)
-    search_fields = ('user__username',)
+from django.contrib import admin
+‎from .models import Category, DiagnosticEntry, DiagnosticHistory
+‎
+‎
+‎@admin.register(Category)
+‎class CategoryAdmin(admin.ModelAdmin):
+‎    list_display = ('id', 'name')
+‎    search_fields = ('name',)
+‎
+‎
+‎@admin.register(DiagnosticEntry)
+‎class DiagnosticEntryAdmin(admin.ModelAdmin):
+‎    list_display = ('name', 'category', 'region', 'created_at')
+‎    list_filter = ('category',)
+‎    search_fields = ('name', 'symptoms', 'causes')
+‎
+‎
+‎@admin.register(DiagnosticHistory)
+‎class DiagnosticHistoryAdmin(admin.ModelAdmin):
+‎    list_display = ('user', 'diagnostic_entry', 'category', 'timestamp')
+‎    list_filter = ('category',)
+‎    search_fields = ('user__username',)
+‎
