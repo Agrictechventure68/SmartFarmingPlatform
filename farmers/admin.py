39f7e2e9f05d755b97ded7ae917051595e9ebdff
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-03-10 09:01:07 +0100

    Update admin.py

--- farmers/admin.py
+++ farmers/admin.py
@@ -4,4 +4,4 @@ from .models import Farmer
 @admin.register(Farmer)
 class FarmerAdmin(admin.ModelAdmin):
       list_filter = ('farm_type',)
-search_fields = ('name', 'location', 'phone') ✅ replace fields with actual ones from Farmer model
+search_fields = ('name', 'location', 'phone')  replace fields with actual ones from Farmer model
