a2ced5c55fe4466c0a17024a1bf344977d49b433
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-03-10 09:20:55 +0100

    Update admin.py

--- farmers/admin.py
+++ farmers/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Farmer
 
+
 @admin.register(Farmer)
 class FarmerAdmin(admin.ModelAdmin):
-      list_filter = ('farm_type',)
-search_fields = ('name', 'location', 'phone')  replace fields with actual ones from Farmer model
+    list_display = ('id', 'name', 'location', 'phone', 'farm_type')
+    search_fields = ('name', 'location', 'phone')
